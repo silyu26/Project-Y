1fc22b1f522845a03b4e82bdea862642806dc142
Silyu Li <silyu.li@rwth-aachen.de>
2024-02-05 18:31:53 +0100

    Finish error handling and data selection

--- src/components/correlations/connection.js
+++ src/components/correlations/connection.js
@@ -1,13 +1,18 @@
 import { useSession } from "@inrupt/solid-ui-react";
-import { getSolidDataset, getFile } from "@inrupt/solid-client";
+import { getSolidDataset, getFile, isContainer } from "@inrupt/solid-client";
 import { useEffect, useState } from "react";
 import { checkHeartRateStatus, checkTemperatureStatus, checkHydrationStatus } from "../../utils/normalRanges";
 import CorrelationMatrixComponent from "./matrix";
 import GraphVisualizeComponent from "./graphVisualize";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col, Button, Modal, Toast, ToastContainer } from "react-bootstrap";
 import { Status } from "../../utils/normalRanges";
 import Goals from "../../pages/goals";
 import { parseNumberFromString } from "../../utils/parser";
+import makeAnimated from 'react-select/animated';
+import Select from 'react-select';
+import { FaRegFaceSmileWink } from "react-icons/fa6";
+
+const animatedComponents = makeAnimated()
 
 const PodConnectionSuggestion = () => {
 
@@ -17,8 +22,37 @@ const PodConnectionSuggestion = () => {
   const [heartrateArr, setHeartrateArr] = useState(null)
   const [bodyTempArr, setBodyTempArr] = useState(null)
   const [queriedDataset, setQueriedDataset] = useState(null)
+  const [containers, setContainers] = useState([])
+  const [validContainer, setValidContainer] = useState([])
+  const [isSharing, setIsSharing] = useState(false)
+  const [urls, setUrls] = useState([])
+  const [urls2, setUrls2] = useState([])
+  const [show, setShow] = useState(false)
+  const [show2, setShow2] = useState(false)
 
+  useEffect(() => {
+    const getAllContainers = async() => {
+      try {
+        const hrDataset = await getSolidDataset(process.env.REACT_APP_FHIR_DATA_URL, { fetch: session.fetch })
+        console.log(hrDataset)
+        let tmp = []
+        for (const key in hrDataset.graphs.default) {
+          if(isContainer(key) && key!==process.env.REACT_APP_FHIR_DATA_URL ) {
+            const tmpUrl= key.replace(process.env.REACT_APP_FHIR_DATA_URL,"")
+            tmp.push({ value: tmpUrl, label: tmpUrl })
+          }
+        }
+        setContainers(tmp)
+        console.log(tmp)
 
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getAllContainers()
+  }, [])
+
+  /*
   useEffect(() => {
     const getHeartrateSources = async () => {
       try {  //  change following url to the pod container of heartrate and body temperature  fhir/
@@ -51,7 +85,7 @@ const PodConnectionSuggestion = () => {
       }
     }
     getHeartrateSources()
-  }, [session])
+  }, [session]) */
 
   useEffect(() => {
     const queryObj = async () => {
@@ -60,28 +94,21 @@ const PodConnectionSuggestion = () => {
         let tempArr = []
         let hydArr = []
         let activeArr = []
-        if (hrSources.length > 0) {
-          const currentDate3 = new Date()
-          console.log('Start getting obj:', currentDate3.toLocaleTimeString())
-          /*hrSources.forEach(async source => {
-            const file = await getFile(
-              source,        
-              { fetch: session.fetch }   
-            )
-            const obj = JSON.parse(await file.text())
-            objArr.push(obj)
-          })*/
-          const promises = hrSources.map(async (source) => {
-            const file = await getFile(source, { fetch: session.fetch })
-            return JSON.parse(await file.text())
-          })
-          const objArr = await Promise.all(promises)
-          // console.log("res:",objArr)
-          // console.log("1",objArr.length)
-          // console.log("2",objArr[0].measurement.heartrate)
-          const currentDate4 = new Date()
-          console.log('Finish getting obj:', currentDate4.toLocaleTimeString())
-
+        if (urls.length > 0) {
+          for(const url of urls){
+            let sources = []
+            const datasets = await getSolidDataset(url, { fetch: session.fetch })
+            for(const key in datasets.graphs.default){
+              const value = datasets.graphs.default[key]
+              if(!isContainer(value.url)){
+                sources.push(value.url)
+              }
+            }
+            const promises = sources.map(async (source) => {
+              const file = await getFile(source, { fetch: session.fetch })
+              return JSON.parse(await file.text())
+            })
+            const objArr = await Promise.all(promises)
           objArr.forEach(obj => {
             const heartrateObj = {
               value: parseNumberFromString(obj.measurement.heartrate),
@@ -108,6 +135,7 @@ const PodConnectionSuggestion = () => {
             hydArr.push(hydrationObj)
             activeArr.push(sportObj)
           })
+          }
         }
         hrArr.sort((a, b) => a.timestamp - b.timestamp)
         tempArr.sort((a, b) => a.timestamp - b.timestamp)
@@ -122,18 +150,42 @@ const PodConnectionSuggestion = () => {
         const temp = { "temperature": tempArr, "heart rate": hrArr, "hydration": hydArr, "sport": activeArr }
         setQueriedDataset(temp)
         console.log(new Date(), "Done creating the data objects in heartRate.js");
-        console.log(new Date(), temp);
+        console.log(new Date(), temp)
       } catch (error) {
         console.log(error)
       }
     }
     queryObj()
-  }, [hrSources, session])
+  }, [urls, session])
+
+  const shareData = () => {
+    try {
+      let urls = []
+      let urls2 = []
+      if(validContainer.length > 0){
+        console.log(validContainer)
+        for(const container of validContainer){
+          urls.push(process.env.REACT_APP_FHIR_DATA_URL+container.value)
+          urls2.push({ value: container.value, label: container.value })
+        }
+        console.log(urls)
+        setUrls(urls)
+        setUrls2(urls2)
+        setIsSharing(!isSharing)
+      }
+      setShow(true)
+    } catch (error) {
+      console.log(error)
+      setShow2(true)
+    }
+    
+    
+  }
 
   // Example: conditionally render different components based on the route
   const renderContent = () => {
     const currentPath = window.location.pathname
-
+    if(queriedDataset) {
     switch (currentPath) {
       case '/pages/correlation':
         return <GraphVisualizeComponent criteriaData={queriedDataset} />;
@@ -145,6 +197,7 @@ const PodConnectionSuggestion = () => {
         // Default content if the route doesn't match
         return <p>Invalid route</p>;
     }
+    }
   };
 
   return (
@@ -152,10 +205,48 @@ const PodConnectionSuggestion = () => {
       <Container>
       {
         queriedDataset ?
-          renderContent()
-          :
-          <p>Loading</p>
-      }
+        renderContent()
+        :
+        <p>Loading</p>
+      } 
+      <br />
+      <Button variant='outline-primary' onClick={()=>setIsSharing(!isSharing)}>Select Data</Button>
+
+      <Modal show={isSharing} onHide={()=>setIsSharing(!isSharing)} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Data Selection</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <p className='text-center' style={{ fontStyle: 'italic' }}>Simply select the data you want and we'll analyze them for you! <FaRegFaceSmileWink /></p>
+          <Select
+            closeMenuOnSelect={false}
+            components={animatedComponents}
+            defaultValue={urls2}
+            isMulti
+            options={containers}
+            onChange={setValidContainer}
+          />
+          <br/>
+          <Button variant='outline-primary' onClick={shareData}>Start</Button>
+        </Modal.Body>
+      </Modal>
+
+      <ToastContainer position='top-end'>
+      <Toast show={show} onClose={() => setShow(false)} delay={5000} bg='success'>
+        <Toast.Header>
+          <strong className="me-auto">Analyzation Completed!</strong>
+          
+        </Toast.Header>
+        <Toast.Body>You can now view your correlation, suggestion and goals!</Toast.Body>
+      </Toast>
+      <Toast show={show2} onClose={() => setShow2(false)} delay={5000} bg='danger' >
+        <Toast.Header>
+          <strong className="me-auto">Analyzation Failed!</strong>
+          
+        </Toast.Header>
+        <Toast.Body>Something is not working...</Toast.Body>
+      </Toast></ToastContainer>
+
       </Container>
 
   )
